8d6b6e2eb62d18303cd7ab77fa0e8a995051eb88
yuragms <yura.gms@gmail.com>
2022-07-23 14:02:48 +0300

    table

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,29 +1,37 @@
-import { AnimateSharedLayout } from "framer-motion";
+import { motion, AnimateSharedLayout } from "framer-motion";
 import { CircularProgressbar } from "react-circular-progressbar";
 import "react-circular-progressbar/dist/styles.css";
 import React, { useState } from "react";
 import "./Card.css";
+import { UilTimes } from "@iconscout/react-unicons";
+import Chart from "react-apexcharts";
 
 function Card(props) {
   const [expanded, setexpanded] = useState(false);
   // const expanded = false;
   return (
     <AnimateSharedLayout>
-      {expanded ? "Expanded" : <CompactCard param={props} />}
+      {expanded ? (
+        <ExpandedCard param={props} setexpanded={() => setexpanded(false)} />
+      ) : (
+        <CompactCard param={props} setexpanded={() => setexpanded(true)} />
+      )}
     </AnimateSharedLayout>
   );
 }
 
 // CompactCard
-function CompactCard({ param }) {
+function CompactCard({ param, setexpanded }) {
   const Png = param.png;
   return (
-    <div
+    <motion.div
       className="CompactCard"
       style={{
         background: param.color.background,
         boxShadow: param.color.boxShadow,
       }}
+      onClick={setexpanded}
+      layoutId="expandableCard"
     >
       <div className="radialBar">
         <CircularProgressbar
@@ -37,7 +45,75 @@ function CompactCard({ param }) {
         <span>${param.value}</span>
         <span>Last 24 hours</span>
       </div>
-    </div>
+    </motion.div>
+  );
+}
+
+// ExpendedCard
+function ExpandedCard({ param, setexpanded }) {
+  const data = {
+    options: {
+      chart: {
+        type: "area",
+        height: "auto",
+      },
+      dropShadow: {
+        enabled: false,
+        enabledOnSeries: undefined,
+        top: 0,
+        left: 0,
+        blur: 3,
+        color: "#000",
+        opacity: 0.35,
+      },
+      fill: {
+        colors: ["#fff"],
+        type: "gradient",
+      },
+      stroke: {
+        curve: "smooth",
+        colors: ["white"],
+      },
+      tooltip: {
+        x: {
+          format: "dd/MM/yy HH:mm",
+        },
+      },
+      grid: {
+        show: true,
+      },
+      xaxis: {
+        type: "datetime",
+        categories: [
+          "2018-09-19T00:00:00.000Z",
+          "2018-09-19T01:00:00.000Z",
+          "2018-09-19T02:00:00.000Z",
+          "2018-09-19T03:00:00.000Z",
+          "2018-09-19T04:00:00.000Z",
+          "2018-09-19T05:00:00.000Z",
+          "2018-09-19T06:00:00.000Z",
+        ],
+      },
+    },
+  };
+  return (
+    <motion.div
+      className="ExpandedCard"
+      style={{
+        background: param.color.background,
+        boxShadow: param.color.boxShadow,
+      }}
+      layoutId="expandableCard"
+    >
+      <div style={{ alignSelf: "flex-end", cursor: "pointer", color: "white" }}>
+        <UilTimes onClick={setexpanded} />
+      </div>
+      <span>{param.title}</span>
+      <div className="chartContainer">
+        <Chart series={param.series} type="area" options={data.options} />
+      </div>
+      <span>Last 24 hour</span>
+    </motion.div>
   );
 }
 
--- src/components/MainDash/MainDash.jsx
+++ src/components/MainDash/MainDash.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import "./MainDash.css";
 import Cards from "../Cards/Cards";
+import Table from "../Table/Table";
 
 const MainDash = () => {
   return (
     <div className="MainDash">
       <h1>Dashboard</h1>
       <Cards />
+      {/* <h2>Recent Orders</h2> */}
+      <Table />
     </div>
   );
 };
